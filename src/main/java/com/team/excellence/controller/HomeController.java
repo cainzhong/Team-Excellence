54c4d66c1aca27535af32366922fc1dd1a504858
cainzhong <cainzhong@aliyun.com>
2014-12-02 20:40:56 +0800

    commit

---
+++ src/main/java/com/team/excellence/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.team.excellence.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="home.do")
+	public String homePage() {
+		return "home";
+	}
+
+}
